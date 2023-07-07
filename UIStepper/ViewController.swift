a7fab0e47910c8c61c27d8c23559e2ab7b4186ae
Carrington <Manyuchi>
2023-07-07 21:37:50 +0200

    stepper

--- UIStepper/ViewController.swift
+++ UIStepper/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    private let coolStepper: UIStepper = {
+        let stepper = UIStepper(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        stepper.translatesAutoresizingMaskIntoConstraints = false
+        stepper.isContinuous = true
+        stepper.autorepeat = true
+        stepper.wraps = false
+        stepper.stepValue = 1
+        stepper.minimumValue = 0
+        stepper.maximumValue = 10
+        stepper.stepValue = 1
+        stepper.tintColor = UIColor.green
+        return stepper
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(coolStepper)
     }
 
 
